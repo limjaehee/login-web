4e1c11c4c3d09d7f6d0c2e43078eb968a3d476fa
kiki <kiki@bravecompany.io>
2022-03-14 11:22:22 +0900

    키 제거

--- plugins/fireinit.js
+++ plugins/fireinit.js
@@ -1,15 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import key from "./key.js";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA5C7QKeWD0XLi-UkHadbQTw1nxwOMQFBg",
-  authDomain: "test-web-f79f9.firebaseapp.com",
-  projectId: "test-web-f79f9",
-  storageBucket: "test-web-f79f9.appspot.com",
-  messagingSenderId: "817586653459",
-  appId: "1:817586653459:web:8b11c79d450a15f09c72e6",
-};
+const firebaseConfig = key;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
